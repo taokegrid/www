1cca386d72bf255b89d201fff4f7ffb753778c7d
haibinkuang <khb.hnu@gmail.com>
2013-05-11 17:08:05 +0800

    不需要email字段, alipay_account 必须填电子邮箱, 不支持手机号

--- sql/user.sql
+++ sql/user.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS user(
   avatar varchar(255) NOT NULL DEFAULT '' COMMENT '用户头像',
   oauth_uid varchar(255) NOT NULL DEFAULT '' COMMENT 'oauth2 uid',
   oauth_via varchar(32) NOT NULL DEFAULT '' COMMENT 'oauth2 来源',
-  email varchar(64) NOT NULL DEFAULT '' COMMENT 'email',
   alipay_account varchar(64) NOT NULL DEFAULT '' COMMENT '支付宝帐号',
   add_time  DATETIME  NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '商品添加时间',
   PRIMARY KEY (user_id),
